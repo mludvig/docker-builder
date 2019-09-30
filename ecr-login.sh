bbadabfddd4ce8ab0578ccefae6ddd3435783640
Michael Ludvig <mludvig@logix.net.nz>
2019-09-30 15:38:28 +1300

    Simplify ecr-login.sh

--- ecr-login.sh
+++ ecr-login.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -xe
 
+# Optionally give comma-separated registry IDs as an argument.
+# Without an argument logs into the default ECR registry.
+
 if [ -n "$1" ]; then
     export ECR_LOGIN=$1
-    test "$1" == "yes" && ECR_LOGIN=""    # Login to the default AWS account
-    $(aws ecr get-login --no-include-email ${ECR_LOGIN:+--registry-ids $(sed 's/,/ /g' <<< ${ECR_LOGIN})})
+else
+    ECR_LOGIN=""    # Login to the default AWS account
 fi
+$(aws ecr get-login --no-include-email ${ECR_LOGIN:+--registry-ids $(sed 's/,/ /g' <<< ${ECR_LOGIN})})
